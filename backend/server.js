525e85e78ef9e068e3ef0690fbb0a0ad1b6affad
Krein <krein@MacBook-Pro-Krein.local>
2025-09-23 22:10:19 +0300

    fix back

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
-import profileRoutes from "./routes/profileRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js"
 import usersListRoutes from "./routes/usersList.js";
 
+import dotenv from "dotenv";
 dotenv.config();
+
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/uploads", express.static("uploads"));
-
 
 app.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
+
+
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
@@ -29,12 +30,8 @@ app.use("/api/todo", todoRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/users", usersListRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("✅ MongoDB connected");
-    app.listen(process.env.PORT || 4000, () => {
-      console.log(`✅ Server running on http://localhost:${process.env.PORT || 4000}`);
-    });
-  })
-  .catch((err) => console.error("❌ MongoDB error:", err));
+
+
+app.listen(4000, () => {
+  console.log("✅ Server running on http://localhost:4000");
+});
